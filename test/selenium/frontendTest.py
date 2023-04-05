49f9f27ff6dd1d5ad13b2c3eb7ca065bd3f99b52
pawel10.24 <pawel10.24@vp.pl>
2023-04-05 16:07:55 +0000

    update

--- test/selenium/frontendTest.py
+++ test/selenium/frontendTest.py
@@ -3,18 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from os import environ
 from selenium.webdriver.common.by import By
-
+ 
 application_URL = environ.get('APPLICATION_URL', 'http://192.168.44.44:5000/')
 selenium_URL = environ.get('SELENIUM_URL', 'http://192.168.44.44:4444/wd/hub')
-
-
+ 
+ 
 class AppTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        # self.driver = webdriver.Remote(
-        #     command_executor=selenium_URL,
-        #     options=webdriver.FirefoxOptions())
-
+        self.driver = webdriver.Remote(
+            command_executor=selenium_URL,
+            options=webdriver.FirefoxOptions())
+ 
     def test_our_app(self):
         driver = self.driver
         driver.set_page_load_timeout(5)
@@ -27,6 +27,6 @@ class AppTest(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         self.assertNotIn("No results found.", driver.page_source)
         driver.quit()
-
+ 
 if __name__ == "__main__":
     unittest.main()
